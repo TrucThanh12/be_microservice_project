dc160a1955cbdf8923d403be074297196dffbc85
ctv_trucpt <trucdien1812@gmail.com>
2024-03-13 14:18:08 +0700

    build-service

---
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -0,0 +1,37 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.dto.OrderLineItemsDto;
+import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.model.Order;
+import com.example.orderservice.model.OrderlineItems;
+import com.example.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        List<OrderlineItems> orderlineItemsList = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+        order.setOrderlineItemsList(orderlineItemsList);
+        orderRepository.save(order);
+    }
+
+    private OrderlineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderlineItems orderlineItems = new OrderlineItems();
+        orderlineItems.setPrice(orderLineItemsDto.getPrice());
+        orderlineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderlineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderlineItems;
+    }
+}
---
+++ product-service/src/main/java/com/example/productservice/service/ProductService.java
@@ -0,0 +1,42 @@
+package com.example.productservice.service;
+
+import com.example.productservice.dto.ProductRequest;
+import com.example.productservice.dto.ProductResponse;
+import com.example.productservice.model.Product;
+import com.example.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+    private final ProductRepository productRepository;
+    
+    public void createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+        productRepository.save(product);
+        log.info("(createProduct) product: {}",product);
+    }
+
+    public List<ProductResponse> getAllProcucts() {
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
